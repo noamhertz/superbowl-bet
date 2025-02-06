40ae0d495287d68e237ec2cd50c1afe6e73fc48f
noamhertz <hertz.noam@gmail.com>
2025-02-06 11:16:56 +0200

    making chakra work

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Provider } from "@/components/ui/provider";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
         <Provider>{children}</Provider>
       </body>
